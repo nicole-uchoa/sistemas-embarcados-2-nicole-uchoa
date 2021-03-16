c3327939d0ecc2dc0edc821da906e340913f4b6f
nicole-uchoa <nicolle-uchoa@hotmail.com>
2021-03-16 16:23:56 -0300

    prog06

---
+++ Semana02/prog06.py
@@ -0,0 +1,13 @@
+#False Values:
+#False
+#None
+#Zero of any numeric type
+#Any empty sequence. For ex '', (), []
+#Any empty mapping. For ex {}
+
+condition = 'Test'
+
+if condition:
+    print('Evaluated to True')
+else:
+    print('Evaluated to True')
