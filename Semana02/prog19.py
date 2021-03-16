6f65e8b69265078fe93a9c8bac06a336ba53031a
nicole-uchoa <nicolle-uchoa@hotmail.com>
2021-03-16 20:24:25 -0300

    prog19

---
+++ Semana02/prog19.py
@@ -0,0 +1,20 @@
+class Employee():
+    def __init__(self, name, age, salary):
+        self.name = name 
+        self.age = age
+        self.salary = salary
+
+    def __repr__(self):
+        return '({},{},${})'.format(self.name, self.age, self.salary)
+
+from operator import attrgetter 
+
+e1 = Employee('Carl', 37, 70000)
+e2 = Employee('Sarah', 29, 80000)
+e3 = Employee('John', 43, 90000)
+
+employees = [e1,e2,e3]
+
+s_employees = sorted(employees, key=attrgetter('age'))
+
+print(s_employees)
