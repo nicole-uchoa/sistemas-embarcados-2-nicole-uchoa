d58db383688c8db2e1492cba121472a232ddedc1
nicole-uchoa <nicolle-uchoa@hotmail.com>
2021-03-16 17:10:46 -0300

    prog08

---
+++ Semana02/prog08.py
@@ -0,0 +1,15 @@
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def is_leap(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+def days_in_month(year, month):
+    if not 1 <= month <= 12:
+        return 'Invalid Month'
+
+    if month == 2 and is_leap(year):
+        return 29
+
+    return month_days[month]
+
+print(days_in_month(2017, 2))
